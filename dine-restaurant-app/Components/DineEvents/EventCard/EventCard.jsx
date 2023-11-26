480f3584383d199875e44f8ea722cb08f87052e6
grischat <kosolapg2@gmail.com>
2023-11-27 00:58:34 +0100

    -50% of work for event card is done

--- dine-restaurant-app/Components/DineEvents/EventCard/EventCard.jsx
+++ dine-restaurant-app/Components/DineEvents/EventCard/EventCard.jsx
@@ -1,16 +1,31 @@
 import "./EventCard.scss";
-import eventsData from './Events.json'
+import eventsData from "./Events.json";
+import { useState } from "react";
+
 const EventCard = () => {
-    const event = eventsData[0]
+  const initialEventId = eventsData[0].id;
+  const [selectedEventId, setSelectedEventId] = useState(initialEventId);
+
+  const handleEventClick = (eventId) => setSelectedEventId(eventId);
+
+  const selectedEvent = eventsData.find((event) => event.id === selectedEventId);
+    
   return (
     <div className="events__list">
-      <img src={event.img} alt="" />
-      {eventsData.map(event => (
-        <span key={event.id}>{event.header}</span>
+      <img src={selectedEvent.img} alt="" />
+      {eventsData.map((event) => (
+        <span key={event.id} onClick={() => handleEventClick(event.id)}>
+          {event.header}
+        </span>
       ))}
-      <h2 className="events__header">{event.header}</h2>
-      <p className="events__description">{event.description}</p>
+      {selectedEvent && (
+        <>
+          <h2 className="events__header">{selectedEvent.header}</h2>
+          <p className="events__description">{selectedEvent.description}</p>
+        </>
+      )}
     </div>
   );
 };
+
 export default EventCard;
